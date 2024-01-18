a17daf5de20c5ee096ab884c7b32f7e833c460e7
tedpet0423 <h23tedpe@du.se>
2024-01-18 11:39:10 +0100

    färdig uppgift

--- modules.py
+++ modules.py
@@ -1,16 +1,21 @@
 import random
 
 
-
 def div_two_numbers(a, b):
     """
     Detta är en funktion för att hitta de heltal som både är delbart med a & b.
     Vi vill veta om 'number' är ett heltal som är både jämnt delbart med 'a' och 'b'.
     Om detta är sant innebär det att 'number' uppfyller kriterierna för båda delningarna.
     """
+    number_list = []
     for number in range(1600):  # Loopar igenom alla heltal från 1-1600#
+
         if number > 0 and (number % a) == 0 and (number % b) == 0:
-            print(str(number))
+            number_list.append(number)
+    if len(number_list) == 0:
+        print('No numbers found :(')
+    else:
+        print(number_list)
 
 
 def guesses_made():
@@ -27,14 +32,15 @@ def guesses_made():
     while keep_loop_going:
         guess = int(input('Take a guess: '))
         guesses_num += 1 # ökar variabelns värde med 1 tills användaren svarar rätt eller når maxantalet för gissningar
-        if guesses_num == 6:
+        if guess == number:
+            print(f'Nice work! You made {guesses_num} guesses to find the right number.')
+            keep_loop_going = False  # Avslutar loopen om användaren gissar rätt
+        elif guesses_num == 6:
             print(f'Oh bummer. The number I was thinking of was {number}')
             keep_loop_going = False  # Avslutar loopen om användaren gjort sex gissningar
         elif guess < number:
             print('To low! Please try again: ')
         elif guess > number:
             print('To high! Please try again: ')
-        elif guess == number:
-            print(f'Nice work! You made {guesses_num} guesses to find the right number.')
-            keep_loop_going = False  # Avslutar loopen om användaren gissar rätt
+
 
--- program.py
+++ program.py
@@ -1,6 +1,8 @@
 import modules
-loop_running = True
+import time
 
+
+loop_running = True
 while loop_running:
     print('Choose an option from list below:'
           '\n1. Print numbers between 1-1600 that can be'
@@ -10,17 +12,20 @@ while loop_running:
     user_input = input()
     if user_input == '1':
         print('You choose 1!')
-        user_input_div_1 = input()
-        user_input_div_2 = input()
-        modules.div_two_numbers(user_input_div_1,user_input_div_2)  # anropar funktionen för att hitta delbara tal
-        input('Press any key to continue')
+        user_input_div_1 = int(input('Type a number between 1-1600: '))
+        user_input_div_2 = int(input('Type another number between 1-1600: '))
+        modules.div_two_numbers(user_input_div_1, user_input_div_2)  # anropar funktionen för att hitta delbara tal
+        input('Press enter to continue')
     elif user_input == '2':
         print('You choose 2!')
         modules.guesses_made()  # anropar funktionen för gissningsspelet
-        input('Press any key to continue')
+        input('Press enter to continue')
     elif user_input == '3':
         print('Exiting...')
+        time.sleep(1)
         loop_running = False
     else:
         print('Try a number from the list!')
-print('Close program Y/N')
+print('Press enter to exit.')
+input()
+exit()
